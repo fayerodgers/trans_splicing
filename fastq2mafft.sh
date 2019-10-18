70fd4a4622cbaab060ecde821560bfb8d7086adb
Faye Rodgers <fr7@sanger.ac.uk>
2019-10-18 12:49:09 +0100

    Try new schisto

--- fastq2mafft.sh
+++ fastq2mafft.sh
@@ -1,5 +1,5 @@
 cat $1  | paste - - - - | cut -f 1-2 |  sed -e 's/^@/>/' | tr '\t' '\n' > ${1}.fa
 
-seqtk sample -s100 ${1}.fa 1000 > 1000_random_sample.fa
+seqtk sample -s100 ${1}.fa 1000 > ${1}_sample.fa
 
-mafft --auto --clustalout 1000_random_sample.fa > 1000_reads_sample.clustal
+mafft --auto --clustalout ${1}_sample.fa > ${1}_sample.clustal
