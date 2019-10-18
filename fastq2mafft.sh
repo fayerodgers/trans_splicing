f4111f706380d18083faf7338e159f2e4101bf80
Faye Rodgers <fr7@sanger.ac.uk>
2019-10-18 12:24:54 +0100

    Make sampling random

--- fastq2mafft.sh
+++ fastq2mafft.sh
@@ -1,3 +1,5 @@
-cat $1 | head -n 4000 | paste - - - - | cut -f 1-2 |  sed -e 's/^@/>/' | tr '\t' '\n' > 1000_reads_sample.fa
+cat $1  | paste - - - - | cut -f 1-2 |  sed -e 's/^@/>/' | tr '\t' '\n' > ${1}.fa
 
-mafft --auto --clustalout 1000_reads_sample.fa > 1000_reads_sample.clustal
+seqtk sample -s100 ${1}.fa 1000 > 1000_random_sample.fa
+
+mafft --auto --clustalout 1000_random_sample.fa > 1000_reads_sample.clustal
