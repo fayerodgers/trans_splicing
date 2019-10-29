bf1185e1f9888f94371624668b56f35a22e69e48
fayerodgers <f.rodgers11@gmail.com>
2019-10-29 09:18:30 +0000

    Update generate_alignments.sh

--- generate_alignments.sh
+++ generate_alignments.sh
@@ -6,8 +6,8 @@ done > "cluster_${2}.fasta"
 i=0
 
 while read -r line; do 
-		echo $line | sed -e "s/>.*/>${i}/" 
-		i=$((i + 1))
+	echo $line | sed -e "s/>.*/>${i}/" 
+	i=$((i + 1))
 done > "cluster_${2}_renamed.fasta"  < "cluster_${2}.fasta"
 
 mafft --auto --clustalout cluster_${2}_renamed.fasta > cluster_${2}.aln
